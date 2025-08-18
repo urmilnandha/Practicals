dcba7394ef7bdae37128633b357e601b84c07625
urmilnandha <urmilnandha0@gmail.com>
2025-08-18 17:45:00 +0530

    Assignments

---
+++ Mathematica.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,b,c,d,e,f;
+printf("Enter any two numbers ");
+scanf("%f %f",&a,&b);
+c=a+b;
+printf("Addition of two number is %f\n",c);
+d = a - b;
+printf("Substraction of numbers is %f\n",d);
+e = a * b;
+printf("Multiplication of two number is %f\n",e);
+f = a / b;
+printf("Division of the numbers is %f\n",f);
+}
